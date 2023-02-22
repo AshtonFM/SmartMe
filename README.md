cf248fd147d2c359c484cb7310bef8fed16b4935
Ashton <96876950+AshtonFM@users.noreply.github.com>
2023-02-22 06:42:38 +0000

    Added source code.

---
+++ README.md
@@ -0,0 +1,11 @@
+# SmartMe
+Developed by AshtonFromUK
+# What is SmartMe?
+SmartMe is a simple command line game that I wrote because I was bored.
+
+Feel free to play it if you are bored it is very simple trust me.
+
+# Links
+Also feel free to follow my [GitHub](https://github.com/AshtonFM)
+
+Also this project is now on github along with all my other command line games. [Project](https://github.com/AshtonFM/SmartMe)
---
+++ index.js
@@ -0,0 +1,118 @@
+#!/usr/bin/env node
+
+import chalk from "chalk";
+import inquirer from "inquirer";
+import gradient from "gradient-string";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import { createSpinner } from "nanospinner";
+
+let pName;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome() {
+    const rainbowTitle = chalkAnimation.rainbow(
+        'Are You Smart Or Dumb?'
+    );
+
+    await sleep();
+    rainbowTitle.stop();
+    console.log(`
+    ${chalk.bgYellow('The Basics')}
+    I am now living in your computer.
+    If you get any question wrong I will be turned off also known as being ${chalk.bgRed('killed')} to my people.
+    So please try your hardest.`)
+}
+
+async function askName() {
+    const answers = await inquirer.prompt({
+        name: 'player_name',
+        type: 'input',
+        message: 'Firstly what is your name?',
+        default() {
+            return 'User';
+        },
+    });
+
+    pName = answers.player_name;
+}
+
+async function question1() {
+    const answers = await inquirer.prompt({
+        name: 'question_1',
+        type: 'list',
+        message: 'How many people created me? is it\n',
+        choices: [
+            'No one',
+            'One person',
+            'Three people',
+            'Four people',
+            'Two people',
+        ],
+    });
+
+    return handleAnswer(answers.question_1 == 'One person');
+    return handleAnswer(answers.question_1);
+}
+
+async function question2() {
+    const answers = await inquirer.prompt({
+        name: 'question_2',
+        type: 'list',
+        message: 'Are you\n',
+        choices: [
+            'Smart',
+            'Dumb',
+        ],
+    });
+
+    return handleAnswer(answers.question_2 == 'Dumb');
+    return handleAnswer(answers.question_2);
+}
+
+async function question3() {
+    const answers = await inquirer.prompt({
+        name: 'question_3',
+        type: 'list',
+        message: 'How long did it take to create JavaScript is it\n',
+        choices: [
+            '10 days',
+            '4 days',
+            '5 days',
+            '2 weeks',
+            '1 week 4 days', 
+        ],
+    });
+
+    return handleAnswer(answers.question_3 == '10 days');
+    return handleAnswer(answers.question_3);
+}
+
+async function handleAnswer(isCorrect) {
+    const spinner = createSpinner('Checking the answer you entered...').start();
+    await sleep();
+
+    if (isCorrect) {
+        spinner.success({ text: `Great job ${pName}. your answer is correct` });
+    } else {
+        spinner.error({ text: `How could you ${pName} I thought we was friends... I- I am ${chalk.bgRed(`Dying`)}.`});
+        process.exit(1);
+    }
+}
+
+function winnerwinnerchickendinner() {
+    console.clear();
+    const msg = `Yay! ${pName} you completed this without killing me here is so money for your troubles!\n £ 1`;
+
+    figlet(msg, (err, data) => {
+        console.log(gradient.pastel.multiline(data));
+    });
+}
+
+await welcome();
+await askName();
+await question1();
+await question2();
+await question3();
+await winnerwinnerchickendinner();
